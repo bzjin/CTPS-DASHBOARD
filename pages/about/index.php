2c9885bff621fc30b7f1e914c9ce936980bf750e
Ben Krepp <bkrepp@ctps.org>
2016-11-15 08:47:31 -0500

    Updated text on "about" page.
    Updated text for "about" page. Note that although this text has been
    sent to Anne McGahan for review, she hasn't yet reviewed it, nor  has it
    been trhough our editing process. So, this text is likely to change
    again.

--- pages/about/index.php
+++ pages/about/index.php
@@ -37,55 +37,64 @@
 	<div id="about" class="col-md-12">
 		<h1>About</h1>
 
-		<p> Every MPO in the US is being required by the Federal Government to define performance metrics and goals for their transportation system, and track progress with respect to those goals over time. This dashboard was developed not only to satisfy those federal requirements, but also share these metrics and goals with the public. Through data visualization, the Boston MPO hopes to show an accurate, navigable, and interactive picture of the region.</p>
+		<p>
+		The Federal Government is requiring every MPO in the United States to define performance metrics and goals for their transportation system, 
+		and to track progress with respect to those goals over time. 
+		This dashboard was developed to meet those requirements, and to share this information with the general public share these metrics and goals with the public. 
+		Through data visualization, the Boston MPO hopes to show an accurate, navigable, and interactive picture of the state of the region’s transportation system.
+		</p>
 
-		<p> Hordes of raw and processed data collected over the past decades have been curated to fit the visual displays in this dashboard. The following text describes the data collection process, raw data structure, and offline processing/calculations that were done to parse through hundreds of megabytes of features and attributes. </p>
+		<p> 
+		The raw data on the region’s transportation system, collected over more than a decade, has been curated forms suitable for visual display in this dashboard. 
+		The following text describes the sources of data for this dashboard, and the processing done to convert the raw data for use in the dashboard.
+		</p>
 
 		<h2> Crashes </h2>
-		<p> Crash data was obtained from the Massachusetts Registry of Motor Vehicles (RMV) Crash Data System.
-		The RMV collects crash data from the Massachusetts State Police, the police departments of individual cities and towns, and from motor vehicle operators.
-		The data submitted by operators has not been entered into the RMV CDS for several years because of a shortage of staff.
+		<p> 
+		Crash data was obtained from the Massachusetts Registry of Motor Vehicles (RMV) Crash Data System (CDS.) 
+		The RMV collects crash data from the Massachusetts State Police, the police departments of individual cities and towns, and from motor vehicle operators. 
+		The data submitted by operators has not been entered into the RMV CDS for several years because of a shortage of staff. 
 		The completeness of crash data submitted by the police departments of individual cities and towns varies from municipality to municipality.
 		</p>
 
 		<h2> Pavement </h2>
-		<p> Data was extracted from the Massachusetts Road Inventory produced by the Massachusetts Department of Transportation (MassDOT), formerly the Massachusetts
-		    Executive Office of Transporation, for the years 2007 through 2015.
-		    The following processing was performed on each year's Road Inventory:
+		<p> 
+		Data was extracted from the Massachusetts Road Inventory produced by the Massachusetts Department of Transportation (MassDOT), 
+		formerly the Massachusetts Executive Office of Transportation, for the years 2007 through 2015. 
+		The following processing was performed on each year's Road Inventory:
+		</p>
 			<ol>
 				<li> Clip Road Inventory to the MPO boundary.</li>
-				<li> Select either records with NHSStatus = 1 (for interstate NHS pavement condition) or NHSStatus > 1 (for non-interstate NHS pavement condition.)</li>
-				<li> For Interstate routes:
+				<li> For interstate routes:
 					<ol>
-						<li> The data was exported in GeoJSON format, from which the visualization was generated directly.</li>
-						<li> Pavement condition for interstate roads is classified as follows:
-							<ul>
-								<li> Excellent: PSI between 3.5 and 5.0 </li>
-								<li> Good: PSI between 3.0 and 3.5 </li>
-								<li> Fair: PSI between 2.5 and 3.0 </li>
-								<li> Poor: PSI between 0.0 and 2.5 </li>
-							</ul>
-						</li>
+						<li>Select records with NHSStatus = 1</li>
+						<li>Export the data in GeoJSON format, from which the visualization was generated directly.</li>
+						<li>Pavement condition for interstate roads is classified as follows by MassDOT:</li>
+							<ol>
+								<li> Excellent: PSI between 3.5 and 5.0</li>
+								<li> Good: PSI between 3.0 and 3.5</li>
+								<li> Fair: PSI between 2.5 and 3.0</li>
+								<li> Poor: PSI between 0.0 and 2.5</li>
+							</ol>
 					</ol>
 				</li>
-				<li> For non-Interstate routes:
+				<li> For non-interstate routes:
 					<ol>
-						<li> Select records with PSI field value of NULL, and calculate number of lane-miles.</li>
-						<li> Pavement condition for non-interstate roads is classified as follows:
-							<ul>
-								<li> Excellent: PSI between 3.5 and 5.0 </li>
-								<li> Good: PSI between 2.8 and 3.5 </li>
-								<li> Fair: PSI between 2.3 and 2.5 </li>
-								<li> Poor: PSI between 0.0 and 2.3 </li>
-							</ul>						
-						</li>
-						<li> For each PSI classification, select records with PSI specified range of PSI values, and calculate number of lane-miles. </li>
-						<li> Use ArcMap "Summary Statistics" tool to generate total number of lane-miles with a NULL PSI value and with a PSI value in each
-   						     of the four categories, grouped by TOWN and TOWN_ID. </li>
+						<li>Select records with NHSStatus > 1</li>
+						<li>Pavement condition for non-interstate roads is classified as follows by MassDOT:</li>
+							<ol>
+								<li> Excellent: PSI between 3.5 and 5.0</li>
+								<li> Good: PSI between 2.8 and 3.5</li>
+								<li> Fair: PSI between 2.3 and 2.5</li>
+								<li> Poor: PSI between 0.0 and 2.3</li>
+							</ol>
+						<li>For each PSI classification, select records with a PSI value in specified range, and calculate the number of lane-miles.</li>
+						<li>Use ArcMap "Summary Statistics" tool to generate total number of lane-miles with a NULL PSI value and with a PSI value
+   						    in each of the four categories, grouped by municipality.</li>
 					</ol>
 				</li>
 			</ol>
-		</p>
+
 
 		<h2> Bridges </h2>
 		<p> Bridge data was obtained from the Bridge Division of the Massachusetts Department of Transportation's Highway Division Bridge Section
@@ -116,29 +125,29 @@
 				<li> Select records where (RIGHTSIDEWALKWIDTH IS NOT NULL AND RIGHTSIDEWALKWIDTH > 0) OR (LEFTSIDEWALKWIDTH IS NOT NULL AND LEFTSIDEWALKWIDTH > 0) </li>
 				<li> Calculate the number of miles in these selected records, again dividing the value of the Shape_LENGTH field by 1609.344. </li>
 				<li> Use ArcMap "Summary Statistics" tool to generate total number of centerline miles and number of miles with a sidewalk on either or both side of the road,
-				     grouped by TOWN and TOWN_ID.</li>
+				     grouped by municipality.</li>
 			</ol>
 		</p>
 
 		<h2> Bicycle Facilities </h2>
-		<p> Data for on-road bicycle facilities was taken directly from 
-		    <a href="http://www.ctps.org/datacatalog_share/content/bicycle-facilities-municipality"> Bike Facilities data set </a> in the CTPS Data Catalogue. 
-			<!-- NOTE: This data needs to be re-generated directly from the road inventory.
-			           Relevant attributes: marked-shared-lane miles, sign-posted-on-road miles, minimum-four-feet wide shoulders.
-					   -- BK 11/11/2016
-			-->
-		    Data for off-road bicycle facilities was obtained from MAPC.
-			<br/>
-			MORE DETAIL NEEDED HERE.
+		<p>
+			<strong>
+				The “about” text for the bicycle facilities data will need to be re-written, once we’ve got clarification on exactly what bike facility data we’re using.
+			</strong>
 		</p>
 
 		<h2> Demographics</h2>
-		<p> All demographic data was taken from the 2010 Census and then sorted geographically by muncipality and tract. Demographic data specific to the MPO region can be found in the CTPS data catalogue and is linked below.</p>
-		<a href="http://www.ctps.org/datacatalog_share/content/boston-region-mpo-2010-census-demographic-profile">Boston Region MPO 2010 Census Demographic Profile</a>
+		<p>
+			Demographic data was taken from the 2010 United States Census and the 2014 American Community Survey. 
+		</p>
 
-		<h3> A Quick Note on Geometries </h3>
-		<p> For the most part, mapping on this dashboard was done by exporting data in GeoJSON format from ArcMap. This allows various data structures and properties to be encoded into geographies and to be represented and displayed on this dashboard. For the purposes of this dashboard, most <a href="http://geojson.org/">GeoJSONs</a> were then turned into <a href="https://github.com/mbostock/topojson">TopoJSONs</a>, a topology-preserving data format that allows for simplification and faster loading speeds.</p>
-		
+		<h3> A Note on Geometries </h3>
+		<p> 
+		For the most part, mapping on this dashboard was done by exporting spatial data from ArcMap to <a href="http://geojson.org/">GeoJSON</a> format. 
+		Most GeoJSON data was then converted to <a href="https://github.com/mbostock/topojson">TopoJSON</a> format, 
+		a topology-preserving spatial data format, in order to improve page load-time performance.
+		</p>
+
 	</div>
 		
 	<div class="footer col-md-12">
