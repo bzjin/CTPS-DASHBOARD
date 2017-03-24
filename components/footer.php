e4959cbd755c3048831ccdc5b6941637bc280801
Ben Krepp <bkrepp@ctps.org>
2017-03-24 14:20:13 -0400

    Small tweak to footer - make URLs relative.
    Small tweak to footer - make URLs relative.

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,3 @@
 <?php 
-	echo '<p>Presented by <a href="http://www.ctps.org/">Boston Region Metropolitan Planning Organization</a> | <a href="https://twitter.com/BostonRegionMPO?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fa fa-twitter" aria-hidden="true"></i></a> | Return to the <a href="http://ctps.org/performance">Performance Based Planning Page</a></p>';
+	echo '<p>Presented by <a href="/">Boston Region Metropolitan Planning Organization</a> | <a href="https://twitter.com/BostonRegionMPO?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fa fa-twitter" aria-hidden="true"></i></a> | Return to the <a href="/performance">Performance Based Planning Page</a></p>';
 ?>
